2b7f4f4745e3e6dc0725a1a6517913b3903ed346
Samit Koyom <samitkoyom@gmail.com>
2023-02-09 15:24:12 +0700

    initial commit

---
+++ reactjs/src/App.js
@@ -0,0 +1,34 @@
+import {useState, useEffect} from 'react'
+import axios from 'axios'
+const baseURL = "http://localhost:4000/api/products"
+
+function App() {
+
+  const [products, setProducts] = useState(null)
+
+  useEffect(() => {
+    axios.get(baseURL).then((response) => {
+      setProducts(response.data)
+    })
+  }, [])
+
+  if (!products) return null
+
+  return (
+    <div>
+          {
+          products.map(element => {
+              return (
+                <div key={element.id} >
+                  <h2>{element.productName}</h2>
+                  <p>{element.price}</p>
+                  <p>{element.thumbnail}</p>
+                </div>
+              )
+            })
+          }
+    </div>
+  )
+}
+
+export default App
